f86e5b276fa824ef5ea3eb35ab421e5b2e1e75cf
Chris Williams <chrismwilliams@users.noreply.github.com>
2022-08-23 19:16:46 +0100

    convert to a simple astro app

---
+++ astro.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "astro/config";
+import UnoCSS from "unocss/astro";
+import { presetUno } from "unocss";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    UnoCSS({
+      presets: [presetUno()],
+    }),
+  ],
+});
